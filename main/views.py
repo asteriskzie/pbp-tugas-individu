22f820e49543250b4f8416d33e2e7c0331231e41
asteriskzie <ester.gracia.10@gmail.com>
2023-09-10 20:50:56 +0700

    Style main.html

--- main/views.py
+++ main/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Pak Bepe',
-        'class': 'PBP A'
+        'name': 'Alarm Clock',
+        'amount': 10, 
+        'description': 'Ready to wake u up to reality.'
     }
 
     return render(request, "main.html", context)
