19b94a8d51bbf66c57388cd1d4efb87adfae1111
asteriskzie <ester.gracia.10@gmail.com>
2023-09-25 12:41:18 +0700

    Implement register, login, and logout

--- main/views.py
+++ main/views.py
@@ -10,8 +10,14 @@ from django.core import serializers
 
 from django.db.models import Sum
 
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages  
 
-# Create your views here.
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='/login')
 def show_main(request):
     items = Item.objects.all()
     
@@ -25,7 +31,6 @@ def show_main(request):
         'items' : items,
         'sum': sum,
     }
-
 
     return render(request, "main.html", context)
 
@@ -61,3 +66,32 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = Item.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request) :
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main:show_main')
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('main:login')
